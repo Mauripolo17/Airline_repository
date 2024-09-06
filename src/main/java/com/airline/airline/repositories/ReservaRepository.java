414868cc9a23ebbc361a41c392d1313bf94f090b
Mauricio Polo <mauriciopoloem@gmail.com>
2024-09-05 22:37:52 -0500

    Se agregaron repositorios

--- src/main/java/com/airline/airline/repositories/ReservaRepository.java
+++ src/main/java/com/airline/airline/repositories/ReservaRepository.java
@@ -1,4 +1,10 @@
 package com.airline.airline.repositories;
 
-public interface ReservaRepository {
+import com.airline.airline.entities.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
 }
--- src/main/java/com/airline/airline/repositories/VueloRepository.java
+++ src/main/java/com/airline/airline/repositories/VueloRepository.java
@@ -1,4 +1,10 @@
 package com.airline.airline.repositories;
 
-public interface VueloRepository {
+import com.airline.airline.entities.Vuelo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VueloRepository extends JpaRepository<Vuelo, Long> {
+
 }
